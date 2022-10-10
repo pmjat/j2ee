440cd622bac52a0c4f48be797508a7bf23aa0c4c
PM Jat <pmjatz@gmail.com>
2022-10-10 08:27:35 +0530

    Upload PollSimulatorWeb - MVC
    Upload PollSimulatorWeb - MVC

---
+++ j2ee/PollSimulator/src/main/java/pmj/j2ee/pollsimulator/model/Poll.java
@@ -0,0 +1,51 @@
+package pmj.j2ee.pollsimulator.model;
+
+import java.util.HashMap;
+
+public class Poll {
+
+    public void vote(int candidate_id) {
+        Candidate c = candidates.get( candidate_id );
+        c.voteTo();
+        candidates.put(candidate_id, c);
+    }
+
+    public void setCandidates( String [] names ) {
+
+        Candidate c;
+        for (String cname : names ) {
+            c = new Candidate( cname );
+            candidates.put(c.getID(), c);
+        }
+        
+    }
+    
+    public int getNumberOfCandidates() {
+        return candidates.size();
+    }
+    
+    public String getWinner() {  //returns the candidate name
+        
+        String winner_candidate;
+
+        Candidate[] cands = this.getCandidates();
+        int wc = -1, max_vote = -1;
+        for(Candidate c : cands ) {
+            if ( c.getVoteCount() > max_vote )
+                wc = c.getID();
+        }
+        winner_candidate = candidates.get(wc).getName();           
+        return winner_candidate;
+
+    }        
+    
+    public Candidate[] getCandidates() {
+        Candidate[] cands = new Candidate[candidates.size()];
+        int i = 0;
+        for (HashMap.Entry<Integer,Candidate> c : candidates.entrySet()) 
+            cands[i++] = c.getValue();
+        return cands;
+    }   
+    private final HashMap<Integer, Candidate> candidates = new HashMap<>();
+    
+}
